8bae66433134208daa9f3c6d48bb7c1c91963959
Awadh Patel <awadhpatel177@gmail.com>
2025-04-02 22:12:46 +0530

    Ass1

---
+++ JAVASCRIPT/first.js
@@ -0,0 +1,13 @@
+// create a object where define the details of a class
+
+let Class_Details = {
+    Number_Of_Students : 50 ,
+    Coordinator_Name : "Mr. Patel" ,
+    Hod_Name : "Mr. Jarnail Singh" ,
+    Placed_Students : 25 ,
+    Number_Of_Regular_Students : 15 ,
+    Number_Of_Inregular_Students : 10
+
+}
+
+console.log(Class_Details)
---
+++ JAVASCRIPT/operator.js
@@ -0,0 +1,46 @@
+// let a = 50;
+// let b = 20;
+// let c = 10;
+// let d = 5;
+// let subtract = (a-b-c-d);
+// console.log(subtract)
+
+
+// let a = 5;
+// let subtract = (--a , a-- , a--);
+// console.log(subtract)
+
+
+
+// let a = 50;
+// let b = 5
+// let modules = (a%b);
+// console.log(modules)
+
+
+
+
+// let a = 50;
+// let b = 5;
+// let c = 6;
+// if(a>b && b>c)
+// {
+//     console.log(true)
+// }
+// else
+// {
+//     console.log(false)
+// }
+
+
+let a = prompt("Enter number:");
+let b = prompt("Enter number:");
+let c = prompt("Enter number:");
+if(a>b && b>c)
+    {
+        console.log(true)
+    }
+    else
+    {
+        console.log(false)
+    }
